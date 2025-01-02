4365db1fa86a57bca591f2ef9b69a7bdaba1d0db
Pedro Freitas França <pedrofreifra@gmail.com>
2025-01-02 17:33:32 -0300

    Código de todas as construtoras

--- include/CamadaFisica.h
+++ include/CamadaFisica.h
@@ -11,13 +11,15 @@ class CamadaFisica
         virtual ~CamadaFisica();
         // Codificador de mensagem
         std::vector<bool> gerador_bit_stream(std::string mensagem);
-        // Modula��o Digital
+        // Modulacaoo Digital
         std::vector<int> nzr_polar(std::string bit_stream);
         std::vector<int> manchester(std::string bit_stream);
         std::vector<int> bipolar(std::string bit_stream);
-        // Modula��o por portadora
+        // Modulacao por portadora
         std::vector<int> ask(std::string dig_signal, int a1, int a2, int sample);
-        std::
+        std::vector<int> fsk(std::string dig_signal, int f1, int f2, int sample);
+        std::vector<int> qam_mapping(std::string dig_signal);
+        std::vector<int> qam8_modulation(std::string dig_signal, int sample);
 
     protected:
 
