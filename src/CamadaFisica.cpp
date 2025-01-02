cf3d2a5e4088b410656a8fee4a8eac5eed4a7472
Pedro Freitas França <pedrofreifra@gmail.com>
2025-01-02 17:52:46 -0300

    Update CamadaFisica.cpp

--- src/CamadaFisica.cpp
+++ src/CamadaFisica.cpp
@@ -1,4 +1,19 @@
 #include "CamadaFisica.h"
+#include vector;
+
+
+using namespace std;
+
+vector CamadaFisica::nzr_polar(vector<bool> bit_stream){
+    vector<int> dig_signal;
+    for(bool bit : bit_stream) {
+        if ()bit){
+        dig_signal.append(1);
+        }
+        else:
+                    dig_signal.append(-1)
+    }
+}
 
 CamadaFisica::CamadaFisica()
 {
