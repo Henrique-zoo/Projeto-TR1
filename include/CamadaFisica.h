7b0cbbcb486d15b57a0e7a6c9149edc639920bcc
Henrique Morcelles Salum <Henrique-zoo>
2025-01-02 17:59:38 -0300

    Header Completo

--- include/CamadaFisica.h
+++ include/CamadaFisica.h
@@ -16,10 +16,9 @@ class CamadaFisica
         std::vector<int> manchester(std::vector<bool> bit_stream);
         std::vector<int> bipolar(std::vector<bool> bit_stream);
         // Modulação por portadora
-        std::vector<int> ask(std::string dig_signal, int a1, int a2, int sample);
-        std::vector<int> fsk(std::string dig_signal, int f1, int f2, int sample);
-        std::vector<int> qam_mapping(std::string dig_signal);
-        std::vector<int> qam8_modulation(std::string dig_signal, int sample);
+        std::vector<int> ask(std::vector<int> dig_signal, int a1, int a2, int sample);
+        std::vector<int> fsk(std::vector<int> dig_signal, int f1, int f2, int sample);
+        std::vector<int> psk(std::vector<int> dig_signal, int p1, int p2, int sample);
 
     protected:
 
