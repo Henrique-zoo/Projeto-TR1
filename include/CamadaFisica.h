d9129e4c8d1375e4078282404d84d1d0507145ad
Henrique Morcelles Salum <Henrique-zoo>
2025-01-02 17:36:28 -0300

    Correções

--- include/CamadaFisica.h
+++ include/CamadaFisica.h
@@ -11,13 +11,12 @@ class CamadaFisica
         virtual ~CamadaFisica();
         // Codificador de mensagem
         std::vector<bool> gerador_bit_stream(std::string mensagem);
-        // Modula��o Digital
-        std::vector<int> nzr_polar(std::string bit_stream);
-        std::vector<int> manchester(std::string bit_stream);
-        std::vector<int> bipolar(std::string bit_stream);
-        // Modula��o por portadora
+        // Modulação Digital
+        std::vector<int> nzr_polar(std::vector<bool> bit_stream);
+        std::vector<int> manchester(std::vector<bool> bit_stream);
+        std::vector<int> bipolar(std::vector<bool> bit_stream);
+        // Modulação por portadora
         std::vector<int> ask(std::string dig_signal, int a1, int a2, int sample);
-        std::
 
     protected:
 
